9ad7a88b2b55187a26c69ddf3348aa843facc7d6
David <popadavid843@yahoo.com>
2022-05-26 19:51:17 +0300

    desktop+js-code

---
+++ script.js
@@ -0,0 +1,22 @@
+const dangerText = document.getElementById("danger");
+const dangerButton = document.getElementById("danger-button");
+const inputEmail = document.getElementById("email");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  let valEmail = inputEmail.value;
+
+  if (!validateEmail(valEmail) || valEmail == "") {
+    dangerText.style.opacity = "1";
+    dangerButton.style.opacity = "1";
+    setTimeout(() => {
+      dangerText.style.removeProperty("opacity");
+      dangerButton.style.removeProperty("opacity");
+    }, 2000);
+  }
+});
+
+function validateEmail(valEmail) {
+  return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(valEmail);
+}
